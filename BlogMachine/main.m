c87c45f1903965432177f31628e0711cd79d7936
dazmuda <diana.ucsd@gmail.com>
2012-08-08 13:36:23 -0700

    Initial Commit

---
+++ BlogMachine/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BlogMachine
+//
+//  Created by Diana Zmuda on 8/8/12.
+//  Copyright (c) 2012 Diana Zmuda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
